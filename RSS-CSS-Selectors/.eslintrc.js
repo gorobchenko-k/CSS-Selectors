68a9710368f51cd5839197520b29ace665e740e8
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-16 12:14:19 +0300

    chore: add typescript

--- RSS-CSS-Selectors/.eslintrc.js
+++ RSS-CSS-Selectors/.eslintrc.js
@@ -3,16 +3,49 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['prettier'],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'global-require': 'error',
     'import/extensions': ['error', 'always', { ignorePackages: true }],
+    "import/prefer-default-export": "off",
     'no-use-before-define': ['error', { variables: false }],
     'prettier/prettier': 'error',
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/array-type": [
+        "error",
+        {
+            "default": "array"
+        }
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+            "accessibility": "explicit",
+            "overrides": {
+                "accessors": "explicit",
+                "constructors": "off",
+                "methods": "explicit",
+                "properties": "explicit",
+                "parameterProperties": "explicit"
+            }
+        }
+    ],
+    "max-lines-per-function": [
+        "error",
+        40
+    ],
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/no-non-null-assertion": "error"
   },
 };
