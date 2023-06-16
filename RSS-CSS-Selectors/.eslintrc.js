edac4f900b36789205c849af52ff81d32a59c14e
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-16 11:58:19 +0300

    chore: add prettier

--- RSS-CSS-Selectors/.eslintrc.js
+++ RSS-CSS-Selectors/.eslintrc.js
@@ -3,11 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
+    'global-require': 'error',
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
+    'no-use-before-define': ['error', { variables: false }],
+    'prettier/prettier': 'error',
   },
 };
