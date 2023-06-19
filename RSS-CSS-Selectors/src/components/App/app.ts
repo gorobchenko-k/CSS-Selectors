46f2c31a8fce9769d1821b3f0143b7825a19154d
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-19 13:21:43 +0300

    feat: add app component

---
+++ RSS-CSS-Selectors/src/components/App/app.ts
@@ -0,0 +1,10 @@
+import { Navigation } from '../Navigation/navigation';
+import { Level } from '../Level/level';
+
+class App {
+  private nav = new Navigation();
+
+  private level = new Level();
+}
+
+export { App };
