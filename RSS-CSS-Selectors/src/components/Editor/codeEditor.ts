9c29e47a5ec01a199adfc6f3e4c00832681e1b1d
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-19 13:18:17 +0300

    feat: add codeEditor component

---
+++ RSS-CSS-Selectors/src/components/Editor/codeEditor.ts
@@ -0,0 +1,24 @@
+import './codeEditor.css';
+import { createElement, getElement } from '../../helpers';
+import { StyleList } from '../../types';
+
+const CSS_EDITOR_STYLE: StyleList = {
+  inputAnswer: ['code-editor__input-answer'],
+  button: ['code-editor__button', 'button'],
+};
+
+class CodeEditor {
+  private inputAnswer: HTMLInputElement;
+
+  private button: HTMLButtonElement;
+
+  constructor() {
+    this.button = createElement('button', CSS_EDITOR_STYLE.button, 'Enter');
+    this.inputAnswer = createElement('input', CSS_EDITOR_STYLE.inputAnswer);
+    this.inputAnswer.type = 'text';
+
+    getElement('.code-editor__code').prepend(this.inputAnswer, this.button);
+  }
+}
+
+export { CodeEditor };
