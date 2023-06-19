b5e827397339b4ebe540d97e36ad078c48d77ce1
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-19 13:24:54 +0300

    feat: add LevelData, StyleList and ContentList types

---
+++ RSS-CSS-Selectors/src/types.ts
@@ -0,0 +1,11 @@
+type LevelData = {
+  task: string;
+  boardMarkup: string;
+  selector: string;
+};
+
+type StyleList = Record<string, string[]>;
+
+type ContentList = Record<string, string>;
+
+export { LevelData, StyleList, ContentList };
