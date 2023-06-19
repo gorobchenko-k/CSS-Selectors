f63d801d3b3ffcd508f3b5fc10eab90ac6902b3e
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-19 13:36:56 +0300

    feat: add title task and function for set content

--- RSS-CSS-Selectors/src/components/Level/level.ts
+++ RSS-CSS-Selectors/src/components/Level/level.ts
@@ -1,3 +1,5 @@
+import { levels } from '../../data/levels';
+import { createElement, getElement } from '../../helpers';
 import { CodeEditor } from '../Editor/codeEditor';
 import { CodeViewer } from '../Viewer/codeViewer/codeViewer';
 import { ResultViewer } from '../Viewer/resultViewer/resultViewer';
@@ -8,6 +10,18 @@ class Level {
   private codeViewer = new CodeViewer();
 
   private resultViewer = new ResultViewer();
+
+  private taskTitle: HTMLHeadingElement;
+
+  constructor() {
+    this.taskTitle = createElement('h2', ['level__task']);
+    getElement('.level').prepend(this.taskTitle);
+  }
+
+  public setContent(numberOfLevel: number): void {
+    const levelData = levels[numberOfLevel];
+    this.taskTitle.innerHTML = levelData.task;
+  }
 }
 
 export { Level };
