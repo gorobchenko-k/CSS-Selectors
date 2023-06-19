9d643673a42ff4538f6fb4ce2ed327dcff17a7b2
gorobchenko-k <gorobchenko.es@mail.ru>
2023-06-19 13:24:01 +0300

    feat: add helper functions

---
+++ RSS-CSS-Selectors/src/helpers.ts
@@ -0,0 +1,34 @@
+export const getElement = <TElement extends Element = HTMLElement>(
+  selector: string,
+  scope?: HTMLElement
+): TElement => {
+  const element: TElement | null = (scope || document).querySelector(selector);
+  if (!element) throw new Error(`Element ${selector} does not exist`);
+
+  return element;
+};
+
+export const getElements = <TElement extends Element = HTMLElement>(
+  selector: string,
+  scope?: HTMLElement
+): TElement | NodeList => {
+  const elements: NodeList | null = (scope || document).querySelectorAll(selector);
+  if (!elements) throw new Error(`Elements ${selector} does not exist`);
+
+  return elements;
+};
+
+export const createElement = <K extends keyof HTMLElementTagNameMap>(
+  tagName: K,
+  classes: string[],
+  content?: string,
+  idName?: string
+): HTMLElementTagNameMap[K] => {
+  const element = document.createElement(tagName);
+  if (classes) {
+    classes.forEach((className) => element.classList.add(className));
+  }
+  if (content) element.innerHTML = content;
+  if (idName) element.setAttribute('id', idName);
+  return element;
+};
